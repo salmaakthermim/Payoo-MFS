c1d02a45e94bc0cf70616c6ac68aab96990b9bb2
salmaakthermim <salmaakthermim112@gmail.com>
2024-09-19 15:49:18 +0600

    added alert set

--- JS/login.js
+++ JS/login.js
@@ -3,8 +3,15 @@
 
 document.getElementById('button-login').addEventListener('click', function(event){
     event.preventDefault();
-    console.log('login button clicked');
+    
 
     const MobileNumber = document.getElementById('Mobile-number'). value;
-    console.log(MobileNumber)
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(MobileNumber, pinNumber)
+    if (MobileNumber=== '5' && pinNumber=='1234'){
+        console.log('you are logged in');
+    }
+    else{
+        alert('weong phone number or pin');
+    }
 })
